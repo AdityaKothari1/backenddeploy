2f4085a10a16b107469633715309ba07b8d82a59
AdityaKothari1 <105914134+AdityaKothari1@users.noreply.github.com>
2022-11-07 16:57:39 +0530

    deploy

--- Config/db.js
+++ Config/db.js
@@ -1,4 +1,4 @@
-  const mongoose=require("mongoose")
-  require("dotenv").config()
-  const connection=mongoose.connect(process.env.Mongo_URL)
-  module.exports={connection}
+const moongose=require("mongoose")
+require("dotenv").config()
+const connection=moongose.connect(process.env.Mongo_URL)
+module.exports={connection}
--- index.js
+++ index.js
@@ -1,88 +1,98 @@
 const express=require("express")
+const { connection } = require("./Config/db")
+const { TodoModel } = require("./Model/todo.module")
 const app=express()
 require("dotenv").config()
 const PORT=process.env.PORT||5000
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-var cors = require('cors')
-const { connection } = require("./Config/db");
-const { UserModel } = require("./Model/user.model");
-const { autnentaction } = require("./MIddlware/auth.middleware");
-const { ProjectModel } = require("./Model/project.module");
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
+const cors=require("cors")
+const { UserModel } = require("./Model/user.module")
+const expressip = require('express-ip');
+const { authentaction } = require("./Middleware/auth.middleware")
+app.use(expressip().getIpInfoMiddleware);
 app.use(cors())
 app.use(express.json())
- 
+
+
 app.post("/signup",async(req,res)=>{
-    const {email,password}=req.body
-    const user= await UserModel.findOne({email})
+     const {email,password}=req.body
+     const user=await UserModel.findOne({email})
      if(user){
-          res.send({"msg":"User Already Registered "})
-     }
-     else{
-        bcrypt.hash(password,2, async(err,hash)=>{
-            if(err){
-                res.send({"msg":"Error in Signup"})
-            }
+        res.send({"msg":"User already register"})
+     }else{
+        bcrypt.hash(password,2,async(err,hash)=>{
             const newuser=new UserModel({
                 email:email,
-                password:hash
+                password:hash,
+                ip:req.ipInfo.ip
             })
             await newuser.save()
-            res.send({"msg":"Sign Up Sucessfull"})
+            res.send({"msg":"Sign up sucessfull"})
         })
      }
-  
 })
-
 app.post("/login",async(req,res)=>{
     const {email,password}=req.body
-    const user= await UserModel.findOne({email})
-    console.log(user);
-     const hash=user.password
-     bcrypt.compare(password,hash,async(err,result)=>{
+    const user=await UserModel.findOne({email:email})
+    const hash=user.password
+    bcrypt.compare(password,hash,async(err,result)=>{
         if(result){
-            const token=jwt.sign({email:email},process.env.SECRET_KEY)
-            res.send({"msg":"Login Sucess","token":token})
+            const token=jwt.sign({email},process.env.SECRET_KEY)
+            res.send({"msg":"Login Sucess",token:token})
         }else{
-            res.send({"msg":"Login Failed"})
+            res.send({"msg":"LogIn Failed"})
         }
+    })
+})
+
+app.get("/todos",authentaction,async(req,res)=>{
+     const {email}=req.body
+     const result=await TodoModel.find({email})
+     res.send(result)
+     
+})
+app.post("/todos/add",authentaction,async(req,res)=>{
+     const {email}=req.body
+     console.log(email);
+     const {taskname,status,tag}=req.body
+    
+     const result=new TodoModel({
+         taskname,
+         status,
+          tag,
+         email:email
      })
+     await result.save()
+     res.send({"msg":"Todo Added"})
 })
-  
-app.get("/",async(req,res)=>{
-      const result=await ProjectModel.find()
-      res.send(result)
-})
-app.post("/addproject",autnentaction,async(req,res)=>{
-     const data=req.body
-      await ProjectModel.insertMany(data)
-      res.send({"msg":"Project Added"})
-})
-app.delete("/project/:id", autnentaction,async (req, res) => {
+app.delete("/todos/:id",  async (req, res) => {
     const { id } = req.params;
-    await ProjectModel.deleteOne({ _id: id });
-    res.send({"msg":"Project Deleted"});
-});
-app.put("/project/:id",autnentaction,async (req, res) => {
-    const {id} = req.params;
-    const data = req.body;
-    await TodoModel.deleteOne({_id: id });
-    await TodoModel.insertMany(data);
-    res.send({"msg":"Data Updated"});
+    await TodoModel.deleteOne({ _id:id });
+    res.send("Delete Sucess");
   });
+
+  app.patch("/todos/:id",  async (req, res) => {
+    const { id } = req.params;
+    const {status}=req.body
+    await TodoModel.updateOne({ _id: id },{$set:{status:status}});
+    res.send("update Sucess");
+  });
+
+
+
+
+
+
 
 
 
 app.listen(PORT,async()=>{
     try{
-      await connection
-      console.log("Connection Sucess");
+        await connection
+        console.log("Connection sucess");
     }catch(err){
         console.log(err);
-        console.log("Error in Connection");
+        console.log("Error in connection");
     }
 })
-
-
-
-
